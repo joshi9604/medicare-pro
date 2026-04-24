76f052d637c52fc6c620d73323b069f3f4b50264
Suraj Joshi <surajjoshi55258@gmail.com>
2026-04-24 14:48:39 +0530

    Added

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import App from './App';
 import './styles/index.css';
 
+// Global API Base URL
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.withCredentials = true; // optional
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
